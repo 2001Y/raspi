f7da4675c23463a1480a4980df1a13bbfd5d96c3
2001Y <15167896+2001Y@users.noreply.github.com>
2023-01-10 17:50:59 +0000

    add All

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,3 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
-import { rotate } from "./client/rotate"
-
-import Clock from "./client/Clock"
-
 export default async function Home() {
 
   return (
