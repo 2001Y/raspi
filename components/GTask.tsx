65ccafebaa4b0f1d9073481b6b463efeab84e9fc
2001Y <15167896+2001Y@users.noreply.github.com>
2023-05-03 09:42:35 +0000

    ad All

--- components/GTask.tsx
+++ components/GTask.tsx
@@ -1,56 +1,72 @@
-// "use client"
-import { unstable_getServerSession } from "next-auth/next"
-import { authOptions } from "../pages/api/auth/[...nextauth]"
+import React, { useState, useEffect } from "react";
 
 type Prop = {
-    accessToken: string
-}
-export default async function GTask({ accessToken }: Prop) {
+    accessToken: string;
+};
 
-    // const session = await unstable_getServerSession(authOptions);
+export default function GTask({ accessToken }: Prop) {
+    // ローディング、エラー、データの状態
+    const [loading, setLoading] = useState(true);
+    const [error, setError] = useState(null);
+    const [data, setData] = useState(null);
 
-    // let accessToken = session.user.accessToken;
+    // データを取得するためのuseEffectフック
+    useEffect(() => {
+        // データを取得する非同期関数
+        async function fetchData() {
+            try {
+                // リストを取得
+                let lists = await fetch(
+                    "https://tasks.googleapis.com/tasks/v1/users/@me/lists",
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: "Bearer " + accessToken,
+                            "Content-Type": "application/json",
+                        },
+                    }
+                ).then((response) => response.json());
 
-    // リストを取得
-    let lists = await fetch(
-        'https://tasks.googleapis.com/tasks/v1/users/@me/lists',
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + accessToken,
-                'Content-Type': 'application/json'
-            },
+                // 最初のリストを選択
+                let listId = lists.items[0].id;
+
+                let list = await fetch(
+                    `https://tasks.googleapis.com/tasks/v1/lists/${listId}/tasks?showCompleted=false`,
+                    {
+                        method: "GET",
+                        headers: {
+                            Authorization: "Bearer " + accessToken,
+                            "Content-Type": "application/json",
+                        },
+                    }
+                ).then((response) => response.json());
+
+                const data = list.items;
+                data.sort((a: any, b: any) => a.position - b.position);
+
+                // データを状態にセット
+                setData(data);
+            } catch (error) {
+                // エラーを状態にセット
+                setError(error);
+            } finally {
+                // ローディングをfalseにセット
+                setLoading(false);
+            }
         }
-    ).then((response) => response.json())
-        .catch((error) => {
-            console.error(error);
-        });
+        // 非同期関数を呼び出す
+        fetchData();
+    }, [accessToken]); // 依存配列
 
-    // 最初のリストを選択
-    let listId = lists.items[0].id;
-
-    let list = await fetch(
-        `https://tasks.googleapis.com/tasks/v1/lists/${listId}/tasks?showCompleted=false`,
-        {
-            method: 'GET',
-            headers: {
-                'Authorization': 'Bearer ' + accessToken,
-                'Content-Type': 'application/json'
-            },
-        }
-    ).then((response) => response.json());
-
-    const data = list.items;
-    data.sort((a: any, b: any) => a.position - b.position);
-
-    return (
-        <ul>
-            {data.length != 0 && data.map((e: any, i: number) => (
-                <li key={i}>
-                    {e.title}
-                </li>
-            ))}
-        </ul>
-    )
-
+    // 状態に応じてUIをレンダリング
+    if (loading) return <div>Loading...</div>;
+    if (error) return <div>Error: {error.message}</div>;
+    if (data)
+        return (
+            <ul>
+                {data.length != 0 &&
+                    data.map((e: any, i: number) => <li key={i}>{e.title}</li>)}
+            </ul>
+        );
+    return null;
 }
